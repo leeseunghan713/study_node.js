348755f9213c8d25369fc44391b3cfc16fbb5eae
leeseunghan713 <127086663+leeseunghan713@users.noreply.github.com>
2023-10-02 00:07:21 +0900

    Add files via upload

--- nodejs/systex/conditional.js
+++ nodejs/systex/conditional.js
@@ -1,10 +1,13 @@
+var args = process.argv;
+
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(false){
-    console.log('C1');
-}
-else{
-    console.log('C2');
+
+if(args[2] === '1'){
+  console.log('C1');
+} else {
+  console.log('C2');
 }
 
 console.log('D');
